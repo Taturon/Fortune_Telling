34313cc035c97edbab59a9a77c5f36e522d42912
Taturon <taturon5511@gmail.com>
2020-07-17 18:27:41 +0900

    Add Fortune-Processing into fortune.php

--- fortune.php
+++ fortune.php
@@ -1,6 +1,30 @@
 <?php
+$lots = [1, 2, 3, 4, 5, 6, 7];
 // データが送られて来た場合
 if (isset($_POST['btn'])) {
+	$lot = array_rand($lots);
+	switch ($lot) {
+		case 0:
+			$res = '大吉';
+			break;
+		case 1:
+			$res = '中吉';
+			break;
+		case 2:
+			$res = '小吉';
+			break;
+		case 3:
+			$res = '吉';
+			break;
+		case 4:
+			$res = '半吉';
+			break;
+		case 5:
+			$res = '末吉';
+			break;
+		case 6:
+			$res = '凶';
+	}
 } else {
 	header('Location:button.php');
 	exit();
