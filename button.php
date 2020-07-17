94e93d08823fe7e62a75ca143a1acc90f5c54b57
Taturon <taturon5511@gmail.com>
2020-07-17 18:52:07 +0900

    Add detail.php and Update button.php and fortune.php

--- button.php
+++ button.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 <h1>おみくじ</h1>
-<p>おみくじを引いて今日の運勢を占ってみよう！</p>
+<h2>おみくじを引いて今日の運勢を占ってみよう！</h2>
 <form action="fortune.php" method="POST">
 <input type="submit" name="btn" value="おみくじを引く">
 </form>
--- fortune.php
+++ fortune.php
@@ -1,29 +1,35 @@
 <?php
 $lots = [1, 2, 3, 4, 5, 6, 7];
-// データが送られて来た場合
 if (isset($_POST['btn'])) {
 	$lot = array_rand($lots);
 	switch ($lot) {
 		case 0:
 			$res = '大吉';
+			$num = 1;
 			break;
 		case 1:
 			$res = '中吉';
+			$num = 2;
 			break;
 		case 2:
 			$res = '小吉';
+			$num = 3;
 			break;
 		case 3:
 			$res = '吉';
+			$num = 4;
 			break;
 		case 4:
 			$res = '半吉';
+			$num = 5;
 			break;
 		case 5:
 			$res = '末吉';
+			$num = 6;
 			break;
 		case 6:
 			$res = '凶';
+			$num = 7;
 	}
 } else {
 	header('Location:button.php');
@@ -38,5 +44,8 @@ if (isset($_POST['btn'])) {
 </head>
 <body>
 <h1>結果</h1>
+<h2><?php echo $res; ?></h2>
+<p><a href="detail.php?num=<?php echo $num; ?>">結果の詳細</a></p>
+<p><a href="button.php"><button type="button">もう一度おみくじを引く</button></a></p>
 </body>
 </html>
